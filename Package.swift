12221ddf663aaa334320c2b96bcf0f48f400c516
Anders Tidbeck <anders.tidbeck@gmail.com>
2021-12-14 11:15:40 +0100

    Added macOS version to Package.swift for lsp reasons

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let googleSignInVersion = "6.0.2"
 let package = Package(
   name: "GoogleSignIn",
   defaultLocalization: "en",
-  platforms: [.iOS(.v9)],
+  platforms: [.iOS(.v9), .macOS(.v10_12)],
   products: [
     .library(
       name: "GoogleSignIn",
